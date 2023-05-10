eac3cd1b101d8f40327b8bbd0bf23a0337f5e73c
ehmaddd <ehmaddd@gmail.com>
2023-05-10 12:43:23 +0500

    Solve direct DOM interaction

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,28 +1,25 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../Redux/books/bookSlice';
 
-const getData = () => {
-  const titleField = document.querySelector('#title');
-  const authorField = document.querySelector('#author');
-  const titleText = titleField.value;
-  const authorText = authorField.value;
-  titleField.value = '';
-  authorField.value = '';
-  return {
+const getData = (titleText, authorText) => (
+  {
     title: titleText,
     author: authorText,
-  };
-};
+  }
+);
 
 const AddBook = () => {
   const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
   return (
     <div className="add-book">
       <h1>ADD NEW BOOK</h1>
       <form>
-        <input type="text" id="title" placeholder="Book Title" />
-        <input type="text" id="author" placeholder="Author" />
-        <button type="button" onClick={() => dispatch(addBook(getData()))}>
+        <input type="text" id="title" placeholder="Book Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" id="author" placeholder="Author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+        <button type="button" onClick={() => dispatch(addBook(getData(title, author)))}>
           ADD BOOK
         </button>
       </form>
